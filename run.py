90ab74a0690769dd3ac317cd7f33f9c96c521ca1
Politofr09 <deshidratatorpa@gmail.com>
2024-10-03 21:01:44 +0200

    Reminder that the "secret key" is not secret at all.

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.secret_key = 'your_secret_key_here'
+    app.secret_key = 'your_secret_key_here' # Trust me bro haha
     app.run(debug=True)
